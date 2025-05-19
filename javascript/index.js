5516b10fa8ae98355f465068525daaac258c4fd5
luv <sq.luv1210@gmail.com>
2025-05-19 11:00:09 +0530

    sd

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+
+// console.log("hello")
+// console.log(10-7*3+5)
+
+// console.warn("world")
+// console.error("sfsaf")
+
+// document.write("luv dhameliya")
+
+
+// document.getElementById("red").innerHTML="asssfsf"
---
+++ javascript/operator.js
@@ -0,0 +1,150 @@
+// arthmatic operator
+
+//  + , - , * , / , %  , ++ , -- 
+
+
+// console.log(10+5);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/5);
+// console.log(10%5);
+
+// let a=10;
+// a++;
+// console.log(a)
+
+
+// console.log(++a)
+
+
+// comperison operator
+
+//  < , <= , > , >= , == , != , === , !==
+
+
+// console.log(10 < 50);
+// console.log(10 <= 50);
+// console.log(10 > 5);
+// console.log(10 >= 15);
+// console.log(10 == 5);
+// console.log(10 != 5);
+// console.log(10 === 10);
+// console.log(10 !== 10);
+
+
+// logical operator
+
+//  !  &&  ||
+
+// console.log(10>5 && 10<19)
+// console.log(10>15 && 10<19)
+// console.log(10>5 || 10<19)
+// console.log(10>50 || 10<19)
+
+// let a=20,b=50;
+
+// console.log("before swapping a&b");
+// console.log("A==>",a);
+// console.log("B==>",b);
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// // a=a*b;
+// // b=a/b;
+// // a=a/b;
+
+// // a=a^b;
+// // b=a^b;
+// // a=a^b;
+
+// // c=a;
+// // a=b;
+// // b=c;
+
+// console.log("after swapping a&b");
+// console.log("A==>",a);
+// console.log("B==>",b);
+
+
+// employ salary
+
+// let basicsalary=8000, hra, da, grosssalary;
+
+// hra=basicsalary * 0.10;
+// da=basicsalary * 0.05;
+
+// grosssalary= basicsalary + hra + da;
+
+// console.log("basic salary :", basicsalary);
+// console.log("hra :", hra);
+// console.log("da :", da);
+// console.log("gross salary :", grosssalary);
+
+
+
+// interest
+
+//     let principleofammount = 50000, rate = 1.5, time = 36, interest;
+
+
+// interest = (principleofammount * rate * time) / 100;
+
+
+// console.log("amount=", principleofammount);
+// console.log("rate=", rate);
+// console.log("time=", time);
+// console.log("totalamount=", interest);
+
+
+
+
+// area of triangle
+
+let a , h=10, b=15;
+
+a=(h*b)/2;
+
+console.log("area of triangle=",a);
+
+
+
+
+// area of circle
+
+// let a , pi=3.14 , r=20;
+
+// a= pi*r*r ;
+
+// console.log("area of circle=" ,a);
+
+
+// area of ractangle
+
+// let total ,length=8 , width=5 ;
+
+// total= length*width;
+
+// console.log("area of ractangle=", total);
+
+
+// convert doller into rupees
+
+// let dollar=10 , ruppes=85.52 , total ;
+
+// total= dollar * ruppes;
+
+// console.log("total rupees=", total);
+
+
+// convert feet into inches
+
+// let total ,feet=5, inches=12;
+
+// total=feet*inches;
+
+// console.log("inches is=",total);
+
+
+// convert feet into meters
